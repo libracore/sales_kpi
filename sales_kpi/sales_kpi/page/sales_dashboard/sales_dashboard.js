cd6148995a763dff32dac7f8e336d09690974b3e
lasalesi <larsericmueller@gmail.com>
2018-01-30 17:34:29 +0100

    simple dashboard implemented

--- sales_kpi/sales_kpi/page/sales_dashboard/sales_dashboard.js
+++ sales_kpi/sales_kpi/page/sales_dashboard/sales_dashboard.js
@@ -6,7 +6,7 @@ frappe.pages['sales_dashboard'].on_page_load = function(wrapper) {
 	});
 
 	frappe.sales_dashboard.make(page);
-	frappe.sales_dashboard.run();
+	frappe.sales_dashboard.run(page);
     
     // add the application reference
     frappe.breadcrumbs.add("Sales KPI");
@@ -22,7 +22,132 @@ frappe.sales_dashboard = {
 		$(frappe.render_template('sales_dashboard', data)).appendTo(me.body);
 
 	},
-	run: function() {
-
+	run: function(page) {
+        get_opportunities(page);
+        get_quotations_ytd(page);
+        get_sales_orders_ytd(page);
+        get_sales_invoices_ytd(page);
+        get_quotations_py(page);
+        get_sales_orders_py(page);
+        get_sales_invoices_py(page);
 	}
+}
+
+function get_opportunities(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_opportunities",
+        "callback": function(r) {
+            var container = page.main.find(".opportunity_count").empty();
+            if (r.message.opportunities != 'None') {
+                //window.alert("Opportunities: " + r.message.toSource());
+                $('<h1>' + r.message.opportunities.count + '</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_quotations_ytd(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_quotations_ytd",
+        "callback": function(r) {
+            var container = page.main.find(".quotations_ytd").empty();
+            if (r.message.quotations != 'None') {
+                //window.alert("Opportunities: " + r.message.toSource());
+                //var company = frappe.defaults.get_user_default("company");
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.quotations.sum.toLocaleString() 
+                    + ' (' + r.message.quotations.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_sales_orders_ytd(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_sales_orders_ytd",
+        "callback": function(r) {
+            var container = page.main.find(".sales_orders_ytd").empty();
+            if (r.message.orders != 'None') {
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.orders.sum.toLocaleString() 
+                    + ' (' + r.message.orders.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_sales_invoices_ytd(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_sales_invoices_ytd",
+        "callback": function(r) {
+            var container = page.main.find(".sales_invoices_ytd").empty();
+            if (r.message.invoices != 'None') {
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.invoices.sum.toLocaleString() 
+                    + ' (' + r.message.invoices.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_quotations_py(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_quotations_py",
+        "callback": function(r) {
+            var container = page.main.find(".quotations_py").empty();
+            if (r.message.quotations != 'None') {
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.quotations.sum.toLocaleString() 
+                    + ' (' + r.message.quotations.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_sales_orders_py(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_sales_orders_py",
+        "callback": function(r) {
+            var container = page.main.find(".sales_orders_py").empty();
+            if (r.message.orders != 'None') {
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.orders.sum.toLocaleString() 
+                    + ' (' + r.message.orders.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
+}
+
+function get_sales_invoices_py(page) {
+    frappe.call({
+        "method": "sales_kpi.sales_kpi.page.sales_dashboard.sales_dashboard.get_sales_invoices_py",
+        "callback": function(r) {
+            var container = page.main.find(".sales_invoices_py").empty();
+            if (r.message.invoices != 'None') {
+                var currency = frappe.defaults.get_global_default("currency");
+                $('<h1>' + currency + ' ' 
+                    + r.message.invoices.sum.toLocaleString() 
+                    + ' (' + r.message.invoices.count + ')</h1>').appendTo(container);
+            } else {
+                $('<h1>0</h1>').appendTo(container);
+            }
+        }
+    });
 }
--- sales_kpi/sales_kpi/page/sales_dashboard/sales_dashboard.py
+++ sales_kpi/sales_kpi/page/sales_dashboard/sales_dashboard.py
@@ -5,6 +5,102 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
+from datetime import datetime
 
 def foo():
     pass
+
+@frappe.whitelist()
+def get_opportunities():
+    sql_query = """SELECT COUNT(`name`) AS `count`
+        FROM `tabOpportunity` 
+        WHERE `status` = 'Open' """
+    opportunities = frappe.db.sql(sql_query, as_dict=True)
+    if opportunities:
+        return {'opportunities': opportunities[0] }
+    else:
+        return {'opportunities': 'None' }
+
+@frappe.whitelist()
+def get_quotations_ytd():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabQuotation` 
+        WHERE `docstatus` = 1 AND `transaction_date` >= '{0}-01-01' AND `transaction_date` <= '{1}'""".format(
+        datetime.now().year, 
+        datetime.now().date())
+    print(sql_query)
+    quotations = frappe.db.sql(sql_query, as_dict=True)
+    if quotations:
+        return {'quotations': quotations[0] }
+    else:
+        return {'quotations': 'None' }
+
+@frappe.whitelist()
+def get_sales_orders_ytd():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabSales Order` 
+        WHERE `docstatus` = 1 AND `transaction_date` >= '{0}-01-01' AND `transaction_date` <= '{1}'""".format(
+        datetime.now().year, 
+        datetime.now().date())
+    print(sql_query)
+    orders = frappe.db.sql(sql_query, as_dict=True)
+    if orders:
+        return {'orders': orders[0] }
+    else:
+        return {'orders': 'None' }
+        
+@frappe.whitelist()
+def get_sales_invoices_ytd():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabSales Invoice` 
+        WHERE `docstatus` = 1 AND `posting_date` >= '{0}-01-01' AND `posting_date` <= '{1}'""".format(
+        datetime.now().year, 
+        datetime.now().date())
+    print(sql_query)
+    invoices = frappe.db.sql(sql_query, as_dict=True)
+    if invoices:
+        return {'invoices': invoices[0] }
+    else:
+        return {'invoices': 'None' }
+
+@frappe.whitelist()
+def get_quotations_py():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabQuotation` 
+        WHERE `docstatus` = 1 AND `transaction_date` >= '{0}-01-01' AND `transaction_date` <= '{0}-{1}-{2}'""".format(
+        (datetime.now().year - 1),
+        datetime.now().month, datetime.now().day)
+    print(sql_query)
+    quotations = frappe.db.sql(sql_query, as_dict=True)
+    if quotations:
+        return {'quotations': quotations[0] }
+    else:
+        return {'quotations': 'None' }
+
+@frappe.whitelist()
+def get_sales_orders_py():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabSales Order` 
+        WHERE `docstatus` = 1 AND `transaction_date` >= '{0}-01-01' AND `transaction_date` <= '{0}-{1}-{2}'""".format(
+        (datetime.now().year - 1),
+        datetime.now().month, datetime.now().day)
+    print(sql_query)
+    orders = frappe.db.sql(sql_query, as_dict=True)
+    if orders:
+        return {'orders': orders[0] }
+    else:
+        return {'orders': 'None' }
+        
+@frappe.whitelist()
+def get_sales_invoices_py():
+    sql_query = """SELECT COUNT(`name`) AS `count`, IFNULL(SUM(`base_grand_total`), 0) AS `sum`
+        FROM `tabSales Invoice` 
+        WHERE `docstatus` = 1 AND `posting_date` >= '{0}-01-01' AND `posting_date` <= '{0}-{1}-{2}'""".format(
+        (datetime.now().year - 1),
+        datetime.now().month, datetime.now().day)
+    print(sql_query)
+    invoices = frappe.db.sql(sql_query, as_dict=True)
+    if invoices:
+        return {'invoices': invoices[0] }
+    else:
+        return {'invoices': 'None' }
